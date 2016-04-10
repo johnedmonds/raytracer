368d699d39b58d14b1359cafcacd0f956f34e1b0
John Edmonds <john.a.edmonds@gmail.com>
2016-04-10 14:09:44 -0400

    Add Sphere class.

---
+++ src/math/sphere.rs
@@ -0,0 +1,6 @@
+use math::vec::Vec3;
+
+struct Sphere {
+    center: Vec3,
+    radius: f32,
+}
--- src/math/vec.rs
+++ src/math/vec.rs
@@ -1,26 +1,26 @@
 use std::ops::Add;
 use std::ops::Sub;
 
-struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32,
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl Vec3 {
-    fn dot(&self, other: Vec3) -> f32 {
+    pub fn dot(&self, other: Vec3) -> f32 {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
-    fn cross(&self, other: Vec3) -> Vec3{
+    pub fn cross(&self, other: Vec3) -> Vec3{
         Vec3{
             x: self.y * other.z - self.z * other.y,
             y: self.z * other.x - self.x * other.z,
             z: self.x * other.y - self.y * other.x}
     }
-    fn len(&self) -> f32 {
+    pub fn len(&self) -> f32 {
         (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
     }
-    fn normalized(&self) -> Vec3 {
+    pub fn normalized(&self) -> Vec3 {
         let len: f32 = self.len();
         Vec3{x: self.x / len, y: self.y / len, z: self.z / len}
     }
